c3f1cd0001ed657265e1e0cf38c00d87e60bcddf
Akash Gupta <guptaakashiitg@gmail.com>
2020-12-15 15:20:41 +0530

    added throttling

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
-
+const MAX_REQUEST_COUNT = 500;
 app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
 app.post("/api/image", async (req, res) => {
   try {
+    let curReq = parseInt(
+      await fs.readFile(path.join(__dirname, "./data/request-count.txt"))
+    );
+    if (curReq > MAX_REQUEST_COUNT) {
+      res.status(429).json({ data: "Max request limit exceeded" });
+      return;
+    } else {
+      await fs.writeFile(
+        path.join(__dirname, "./data/request-count.txt"),
+        curReq + 1
+      );
+    }
     const keyFilename = path.join(__dirname, "./config/vision-api-key.json");
     const client = new vision.ImageAnnotatorClient({ keyFilename });
     const imgData = req.body.image;
