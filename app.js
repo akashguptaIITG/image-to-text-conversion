ee7b6d70e6b6bfd852d9a15ad2328ee4e7953371
Akash Gupta <guptaakashiitg@gmail.com>
2020-12-18 15:50:37 +0530

    minor change

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const vision = require("@google-cloud/vision");
-const path = require("path");
-const fs = require("fs").promises;
 const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
-const MAX_REQUEST_COUNT = 500;
+
 app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
