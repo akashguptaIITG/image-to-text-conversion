d966eef349ce9404061cf8920a6c7ea3f5998c8a
Akash Gupta <guptaakashiitg@gmail.com>
2020-12-31 01:49:40 +0530

    minor changes

--- ui/screens/home.jsx
+++ ui/screens/home.jsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 
 export default function App({ navigation }) {
-  const snap = async () => {
+  const openCamera = async () => {
     try {
       const image = await ImagePicker.openCamera({
         width: 300,
@@ -12,9 +12,26 @@ export default function App({ navigation }) {
         freeStyleCropEnabled: true,
         mediaType: 'photo',
         includeBase64: true,
-        compressImageQuality: 0.5,
-      
+        // compressImageQuality: 0.8,
       });
+      await ImagePicker.clean();
+      navigation.navigate('TextRecognition', { image });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const openPicker = async () => {
+    try {
+      const image = await ImagePicker.openPicker({
+        width: 300,
+        height: 400,
+        cropping: true,
+        freeStyleCropEnabled: true,
+        mediaType: 'photo',
+        includeBase64: true,
+        // compressImageQuality: 0.8,
+      });
+      await ImagePicker.clean();
       navigation.navigate('TextRecognition', { image });
     } catch (error) {
       console.error(error);
@@ -24,8 +41,11 @@ export default function App({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={snap}>
+        <TouchableOpacity style={styles.button} onPress={openCamera}>
           <Text style={styles.text}> Capture </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={openPicker}>
+          <Text style={styles.text}> Select from gallery </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -35,26 +55,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  camera: {
-    flex: 6,
-  },
+
   buttonContainer: {
     flex: 1,
-    backgroundColor: 'lightblue',
-    borderWidth: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    margin: 20,
-    marginHorizontal: 130,
-    width: 100,
-    height: 100,
-    borderRadius: 40,
+    justifyContent: 'space-around',
   },
   button: {
-    flex: 1,
-    alignItems: 'center',
-    marginBottom: 30,
+    backgroundColor: 'lightblue',
+    borderWidth: 2,
+    shadowOpacity: 2,
+    padding: 10,
+    borderRadius: 20,
   },
   text: {
     fontSize: 24,
--- ui/screens/text-recognition.jsx
+++ ui/screens/text-recognition.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, Text, Image, ScrollView, View } from 'react-native';
 import axios from 'axios';
 
 export default function TextRecognition({ route }) {
   const [textDetected, setTextDetected] = useState('');
-
+  const image = route.params ? route.params.image : undefined;
+  const source = axios.CancelToken.source();
   useEffect(() => {
     (async () => {
       try {
-        const { image } = route.params;
-        console.log(image);
         const result = await axios.post(
-          'http://52.73.243.18:3000/api/gcp/imageToText',
+          'http://192.168.29.242:3000/api/gcp/imageToText',
           JSON.stringify({ image: image.data }),
           {
             headers: {
@@ -24,18 +23,41 @@ export default function TextRecognition({ route }) {
         setTextDetected(data);
       } catch (error) {
         console.error(error);
+        setTextDetected('no text detected!');
       }
     })();
-  }, []);
+    return () => {
+      source.cancel('Request cancelled');
+    };
+  }, [image]);
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>{textDetected}</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.text}>{textDetected}</Text>
+      </ScrollView>
+      <ScrollView>
+        <Image
+          source={{ uri: `data:image/png;base64,${image.data}` }}
+          style={styles.image}
+        />
+      </ScrollView>
+    </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'flex-start',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
     marginTop: 20,
+    flex: 1,
+  },
+  image: {
+    width: 300,
+    height: 400,
+    marginVertical: 20,
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: '200',
   },
 });
